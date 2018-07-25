4d117221b08effa1ef5fa4698917b9130fbb6769
Sacha Ott <so@chaineum.com>
2018-07-25 17:33:44 +0200

    Initial commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,9 @@
+var SVPToken = artifacts.require('SVPToken');
+
+// NOTE: Use this file to easily deploy the contracts you're writing.
+//   (but make sure to reset this file before committing
+//    with `git checkout HEAD -- migrations/2_deploy_contracts.js`)
+
+module.exports = function (deployer) {
+  deployer.deploy(SVPToken, '0xa5b006cdd7cf51a053ff53f4cad6b0b90458a689');
+};
